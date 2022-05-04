c5f7642968ee78969368ec26601de767ae71a42e
Ilya Tovbin <ilya89@outlook.com>
2022-05-05 02:03:02 +0300

    cleanup

--- background.js
+++ background.js
@@ -3,12 +3,12 @@ import { sget, sset }  from './storageapi.js';
 
 const samlRegex = /name="SAMLResponse" value="([\s\S]+?)"/i;
 const accountSelectionRegex = `tabindex="\\d" jsname="\\S\*" data-authuser="\\d" data-identifier="(\\S\*@DOMAIN)" data-item-index="(\\S)"`;
-const stsTokenSelectionRegex = /<AccessKeyId>(\S+)<.*\n.*<SecretAccessKey>(\S+)<.*\n.*<SessionToken>(\S+)<.*\n.*<Expiration>(\S+)</i
-const google_account_chooser_url = 'https://accounts.google.com/AccountChooser'
-const aws_saml_url = 'https://signin.aws.amazon.com/saml'
-const aws_sts_url = 'https://sts.amazonaws.com'
-const arn_prefix = 'arn:aws:iam::'
-const google_sso_url = 'https://accounts.google.com/o/saml2/initsso?idpid=IDPID&spid=SPID&forceauthn=false&authuser='
+const stsTokenRegex = /<AccessKeyId>(\S+)<.*\n.*<SecretAccessKey>(\S+)<.*\n.*<SessionToken>(\S+)<.*\n.*<Expiration>(\S+)</i
+const googleAccountChooserUrl = 'https://accounts.google.com/AccountChooser'
+const awsSamlUrl = 'https://signin.aws.amazon.com/saml'
+const awsStsUrl = 'https://sts.amazonaws.com'
+const arnPrefix = 'arn:aws:iam::'
+const googleSsoUrl = 'https://accounts.google.com/o/saml2/initsso?idpid=IDPID&spid=SPID&forceauthn=false&authuser='
 
 var alarms = chrome.alarms;
 var props,role;
@@ -43,20 +43,20 @@ main()
 
 
   function refreshAwsTokensInit(){
-    fetch(google_account_chooser_url).then(response=> {
+    fetch(googleAccountChooserUrl).then(response=> {
         response.text().then(accounts=> {
             var re = new RegExp(accountSelectionRegex.replace("DOMAIN",props.organization_domain),"i");
             console.log(`Refreshing credentials for ${accounts.match(re)[1]}`)
             let accountIndex = accounts.match(re)[2]
-            fetch(`${google_sso_url.replace('IDPID',props.google_idpid).replace('SPID',props.google_spid)}${accountIndex}`).then(response => {   
+            fetch(`${googleSsoUrl.replace('IDPID',props.google_idpid).replace('SPID',props.google_spid)}${accountIndex}`).then(response => {   
                 response.text().then(result => {
                     let SAMLReponse=result.match(samlRegex)[1]
             
-                    let roleArn=arn_prefix+role
+                    let roleArn=arnPrefix+role
                     let awsAccount=(roleArn.split(":"))[4]
-                    let principalArn=`${arn_prefix}${awsAccount}:saml-provider/gsuite`
+                    let principalArn=`${arnPrefix}${awsAccount}:saml-provider/gsuite`
                     let data = "RelayState="+"&SAMLResponse="+encodeURIComponent(SAMLReponse)+"&name=&portal=&roleIndex="+encodeURIComponent(roleArn);
-                    fetch(aws_saml_url, {
+                    fetch(awsSamlUrl, {
                         method: "POST",
                         body: data,
                         headers: {
@@ -76,33 +76,7 @@ main()
                         let date = new Date().toLocaleString();
                         console.log(`AWS AlwaysON refreshed tokens successfuly at ${date}`);
                     });
-            
-                    //GET STS Credentials
-                    // let STSUrl = `${aws_sts_url}/?Version=2011-06-15&Action=AssumeRoleWithSAML&RoleArn=${roleArn}&PrincipalArn=${principalArn}&SAMLAssertion=${encodeURIComponent(SAMLReponse.trim())}&AUTHPARAMS&DurationSeconds=${props.session_duration}`
-                    // fetch(STSUrl, {
-                    //     method: "GET",
-                    //     headers: {
-                    //         "Upgrade-Insecure-Requests": "1",
-                    //         "Cache-Control": "max-age=0",
-                    //         "Content-Type": "application/x-www-form-urlencoded",
-                    //         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,"+
-                    //                 "image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-                    //         "Sec-GPC": "1",
-                    //         "Sec-Fetch-Site": "cross-site",
-                    //         "Sec-Fetch-Mode": "navigate",
-                    //         "Sec-Fetch-Dest": "document",
-                    //         "Accept-Encoding": "gzip, deflate, br",
-                    //         "Accept-Language": "en-US,en;q=0.9"
-                    //     }
-                    // }).then(r => r.text()).then(str => new window.DOMParser().parseFromString(str, "text/xml")).then(result => {
-                    //     let accessKeyId=result.getElementsByTagName("AccessKeyId")[0].innerHTML
-                    //     let secretAccessKey=result.getElementsByTagName("SecretAccessKey")[0].innerHTML
-                    //     let sessionToken=result.getElementsByTagName("SessionToken")[0].innerHTML
-                    //     let sessionExpiration=result.getElementsByTagName("Expiration")[0].innerHTML
-                    //     let stsToken = `export AWS_ACCESS_KEY_ID=${accessKeyId} AWS_SECRET_ACCESS_KEY=${secretAccessKey} AWS_SESSION_TOKEN=${sessionToken} AWS_SESSION_EXPIRATION=${sessionExpiration}`
-                    //     sset({'aws_sts_token':stsToken})
-                    // });
-                    let STSUrl = `${aws_sts_url}/?Version=2011-06-15&Action=AssumeRoleWithSAML&RoleArn=${roleArn}&PrincipalArn=${principalArn}&SAMLAssertion=${encodeURIComponent(SAMLReponse.trim())}&AUTHPARAMS&DurationSeconds=${props.session_duration}`
+                    let STSUrl = `${awsStsUrl}/?Version=2011-06-15&Action=AssumeRoleWithSAML&RoleArn=${roleArn}&PrincipalArn=${principalArn}&SAMLAssertion=${encodeURIComponent(SAMLReponse.trim())}&AUTHPARAMS&DurationSeconds=${props.session_duration}`
                     fetch(STSUrl, {
                         method: "GET",
                         headers: {
@@ -119,7 +93,7 @@ main()
                             "Accept-Language": "en-US,en;q=0.9"
                         }
                     }).then((response) => response.text()).then((data) => {
-                        data = data.match(stsTokenSelectionRegex)
+                        data = data.match(stsTokenRegex)
                         let accessKeyId=data[1]
                         let secretAccessKey=data[2]
                         let sessionToken=data[3]
--- readme.md
+++ readme.md
@@ -3,17 +3,18 @@
 - Stay logged in to AWS web console  
 - Get temporary credentials for assumed role to use for CLI access.
 ## Changelog
-v0.1.0 - simple interface and basic POC  
-v0.2.0 - redesigned popup menu ui.  
-v0.3.0 - redesigned communications between frontend and backend. everything is now based on local storage data.  
-v0.3.1 - fixed a milion bugs. only 1.1 million new ones remain.  
-v0.4.0 - added option to get temporary CLI credentials.  
-v0.4.1 - small bugfixes  
-v0.4.2 - fixed session expiration env var  
-v0.5.0 - multiple account support  
-v0.6.0 - added menu. made the extension generic and now it can be uploaded to chrome extension store.  
-v0.6.1 - reduced the amount of permissions to the minimum required in order to work.  
-v0.7.0 - upgraded to manifest V3.  
+v0.1.0   - simple interface and basic POC  
+v0.2.0   - redesigned popup menu ui.  
+v0.3.0   - redesigned communications between frontend and backend. everything is now based on local storage data.  
+v0.3.1   - fixed a milion bugs. only 1.1 million new ones remain.  
+v0.4.0   - added option to get temporary CLI credentials.  
+v0.4.1   - small bugfixes.
+v0.4.2   - fixed session expiration env var.  
+v0.5.0   - multiple account support.
+v0.6.0   - added menu. made the extension generic and now it can be uploaded to chrome extension store.  
+v0.6.1   - reduced the amount of permissions to the minimum required in order to work.  
+v0.7.0   - upgraded to manifest V3.  
+v0.7.0.1 - cleaned up some stuff.
 
 ## To Do:  
 Modify everything to support N number of accounts.  
